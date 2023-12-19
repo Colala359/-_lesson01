1e9b3cf474a4966c34bb13ae9cc49572fc661333
Colala <antt3215@gmail.com>
2023-12-19 19:38:23 +0500

    add lesson01:task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 44;
+int b = 54;
+int c = 23;
+int d = 5;
+int e = 7;
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+if (max < d)
+{
+    max = d;
+}
+if (max < e)
+{
+    max = e;
+}
+Console.WriteLine(max);
